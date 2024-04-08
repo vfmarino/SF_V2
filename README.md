69195b8043bd01cb2ba53a343ab8e50d34b51d14
Victor Marino <vfmarino@gmail.com>
2024-04-08 14:14:32 -0300

    Commit 2

--- README.md
+++ README.md
@@ -6,7 +6,11 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.4.
 
-Terminal: ng new nome-do-projeto
+Terminal:
+
+- ng new nome-do-projeto
+
+- npm run start
 
 Removed Everthing From app.component.html
 
@@ -15,3 +19,22 @@ Add "HELLO WORLD" as a test.
 ## COMMIT 1 
 
 ## STEP 2
+
+Create Component
+Header, footer, home,
+
+Terminal :
+- ng g header
+- ng g footer
+- ng g home
+
+Add route to app-routing.
+example: ({ path: 'footer', component: FooterComponent },)
+
+Add into app.component.html
+<app-header></app-header>
+<router-outlet></router-outlet>
+<app-footer></app-footer>
+
+## COMMIT 2
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
